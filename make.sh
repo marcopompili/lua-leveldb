d53deab06b5a52d62f13285d4f4085f06cb05f47
Marco Pompili <marcs.pompili@gmail.com>
2014-06-22 14:41:56 +0200

    Building based on makefile and rockspec only. No more scripting crap.

--- make.sh
+++ make.sh
@@ -3,48 +3,27 @@
 # Author: Marco Pompili - marcs.pompili@gmail.com
 # Script file for installing the dev version of lua-leveldb.
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
-	echo -e "\e[1;31mUSAGE: $0 <version> <revision>\e[0m"
+	echo -e "\e[1;31mUSAGE: $0 <version> <revision> \e[0;31m[luarocks-path]\e[0m"
+	echo -e "\e[0;32m[luarocks-path] is optional, just use it if you want to use a specific version of Lua.\e[0m"
 	exit 1
+fi
+
+if [ $# -gt 2 ]
+then
+	LUAROCKS=$3
+else
+	LUAROCKS=luarocks
 fi
 
 set -e
 
-LEVELDB_INCLUDE_PATH=/usr/include/leveldb
-CONFIG_HEADER=src/config.h
 VERSION=$1
 REVISION=$2
 
 echo -e "Going for version $1 and revision $2..."
 
-LUAROCKS51="/usr/bin/luarocks-5.1"
-LUAROCKS52="/usr/bin/luarocks-5.2"
-
-echo -e "\e[1:32mChecking lua version:\e[0m"
-
-if [ -f "$LUAROCKS51" ]
-then
-	echo "Found specific lua 5.1 version"
-	LUAROCKS="luarocks-5.1"
-elif [ -f "$LUAROCKS52" ]
-then
-	echo "Found specific lua 5.2 version"
-	LUAROCKS="luarocks-5.2"
-else
-	echo -e "Warning found general lua version"
-	LUAROCKS="luarocks"
-fi
-
 echo -e "\033[0;32mlua-leveldb $VERSION-$REVISION\033[0m"
-echo "Configuring:"
-echo -ne "  *filter_policy.h header found: "
-
-if [ -f "$LEVELDB_INCLUDE_PATH/filter_policy.h" ]
-then
-	echo -e "\033[0;32myes\033[0m"
-else
-	echo -e "\033[0;31mno\033[0m"
-fi
 
 $LUAROCKS make rockspec/lua-leveldb-$VERSION-$REVISION.rockspec
