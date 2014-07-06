9016d478f1f587ba4deef10cb541ce260822f965
Marco Pompili <marcs.pompili@gmail.com>
2014-07-06 16:35:27 +0200

    Moved serializer code from header to cpp file.

--- makefile
+++ makefile
@@ -9,20 +9,18 @@ LUA_LIBDIR=$(LUA_DIR)/lib/lua/5.2
 LUA_INCDIR=/usr/include/lua5.2
 LUA_SHAREDIR=$(LUA_DIR)/share/lua/5.2
 
-
 all: leveldb.so
 
 leveldb.so:
 	@echo -e '\033[0;34mBuilding target: $@\033[0m'
 ifneq (,$(wildcard /usr/include/leveldb/filter_policy.h))
-	$(CXX) src/lua-leveldb.cc $(CXXFLAGS) $(LIBFLAG) -I$(LUA_INCDIR) $(LDFLAGS) -o leveldb.so -DLEVELDB_FILTER_POLICY_H=1
+	$(CXX) src/lua-leveldb.cc src/lvldb-serializer.cpp $(CXXFLAGS) $(LIBFLAG) -I$(LUA_INCDIR) $(LDFLAGS) -o leveldb.so -DLEVELDB_FILTER_POLICY_H=1
 else
-	$(CXX) src/lua-leveldb.cc $(CXXFLAGS) $(LIBFLAG) -I$(LUA_INCDIR) $(LDFLAGS) -o leveldb.so
+	$(CXX) src/lua-leveldb.cc src/lvldb-serializer.cpp $(CXXFLAGS) $(LIBFLAG) -I$(LUA_INCDIR) $(LDFLAGS) -o leveldb.so
 endif
 
 clean:
 	@echo -e '\033[0;33mCleaning binaries:\033[0m'
-	rm -f src/config.h
 	rm -f leveldb.so
 	
 install:
--- src/lvldb-serializer.hpp
+++ src/lvldb-serializer.hpp
@@ -8,6 +8,8 @@
 #ifndef BUFFER_HPP_
 #define BUFFER_HPP_
 
+#include <cstring>
+
 extern "C" {
 #include <stdlib.h>
 #include <stdint.h>
@@ -35,56 +37,10 @@ public:
 		free(data);
 	}
 
-	int set_lua_value(int idx) {
-		int lua_value_type = lua_type(L, idx);
-		//lua_pushvalue(L, idx);
-
-		write((unsigned char*)&lua_value_type, sizeof(int));
-
-		switch(lua_value_type)
-		{
-			case LUA_TBOOLEAN: {
-				int int_val = lua_toboolean(L, -1);
-				write((unsigned char*)&int_val, MAR_CHR);
-				break;
-			}
-			case LUA_TSTRING: {
-				size_t l;
-				const char *str_val = lua_tolstring(L, idx, &l);
-				write((unsigned char*)str_val, l);
-				break;
-			}
-			case LUA_TNUMBER: {
-				lua_Number num_val = lua_tonumber(L, idx);
-				write((unsigned char*)&num_val, MAR_I64);
-				break;
-			}
-			default: {
-				luaL_argerror(L, idx, "Expecting number, string or boolean");
-				return 1; // not executed
-			}
-		}
-
-		return 0;
-	}
-
-	void print_buffer() {
-		print_bytes(data, size);
-	}
-
-	size_t buffer_len() {
-		return data_len;
-	}
-
-	void buffer_data(unsigned char *out) {
-		size_t i;
-
-		for(i = 0; i < data_len; i++)
-			out[i] = data[i];
-
-		//std::cout << "buffer_data\n";
-		//print_bytes(out, data_len);
-	}
+	int set_lua_value(int idx);
+	void print_buffer();
+	size_t buffer_len();
+	void buffer_data(unsigned char *out);
 
 private:
 	lua_State *L;
@@ -94,28 +50,7 @@ private:
 	size_t data_len;
 	unsigned char *data;
 
-	int write(unsigned char *str, size_t len) {
-		if (len > UINT32_MAX)
-			luaL_error(L, "buffer too long");
-
-		if (size - head < len) {
-			size_t new_size = size << 1;
-			size_t cur_head = head;
-			while (new_size - cur_head <= len) {
-				new_size = new_size << 1;
-			}
-			if (!(data = (unsigned char*)realloc(data, new_size)))
-				luaL_error(L, "Out of memory!");
-
-			size = new_size;
-		}
-
-		memcpy(&data[head], str, len);
-		head += len;
-		data_len += len;
-
-		return 0;
-	}
+	int write(unsigned char *str, size_t len);
 
 	void insert_int_big_endian(int n, unsigned char *byte) {
 		byte[0] = (n >> 24) & 0xFF;
