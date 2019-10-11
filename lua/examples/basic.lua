218dc33f4b42f5e60bf1c3ab200fe34f6f61dbb0
Marco Pompili <2620287+marcopompili@users.noreply.github.com>
2019-10-11 12:17:55 +0200

    improve basic example

--- lua/examples/basic.lua
+++ lua/examples/basic.lua
@@ -4,25 +4,26 @@ opt = leveldb.options()
 opt.createIfMissing = true
 opt.errorIfExists = false
 
+local db_file = 'basic.db'
 local test_key = 'key1'
 local test_val = 'val1'
 
-print ('opening test.db')
-testdb = leveldb.open(opt, 'basic.db')
+print ('opening ' .. db_file .. '...')
+local db = leveldb.open(opt, db_file)
 
-if leveldb.check(testdb)
+if leveldb.check(db)
 then
-    if testdb:put(test_key, test_val)
+    if db:put(test_key, test_val)
     then
-        print ('key1: '.. testdb:get(test_key))
+        print ('key1: '.. db:get(test_key))
     end
 end
 
-leveldb.close(testdb)
+leveldb.close(db)
 
-testdb = leveldb.open(opt, 'basic.db')
-testdb:put('key2', '123456')
+db = leveldb.open(opt, db_file)
+db:put('key2', '123456')
 
-print ('key2: ' .. testdb:get('key2'))
+print ('key2: ' .. db:get('key2'))
 
-leveldb.close(testdb)
+leveldb.close(db)
